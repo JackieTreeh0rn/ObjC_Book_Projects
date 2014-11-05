7577d2b2033ab1814f2cbecb2d97c810efc46d83
fsck <fsck66@users.noreply.github.com>
2014-11-04 21:02:55 -0500

    ...

--- ObjC_Constants/ObjC_Constants/main.m
+++ ObjC_Constants/ObjC_Constants/main.m
@@ -11,7 +11,16 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"\u03c0 is %f", M_PI); // example of constant π (Pi) #define constant
-        NSLog(@"%d is larger...", MAX(10, 12)); // example of "#define" constant (used as macro)
+        NSLog(@"%d is larger...", MAX(10, 12)); // example of "#define" constant (used as macro - ie. work as a function)
+        
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:@"currency"]; // global variables are always prefered.  NSLocaleCurrencyCode is a global variable (prefered to using @"currency")
+        NSLog(@"Money is %@", currency);
+        
+        
+        
+        
+        
     }
     return 0;
 }
