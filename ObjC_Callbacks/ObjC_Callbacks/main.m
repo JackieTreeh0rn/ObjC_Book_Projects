fd8da46f25876a2e691b90c18025bbbf89cd0b1f
fsck <fsck66@users.noreply.github.com>
2014-11-14 20:24:23 -0500

    ...

---
+++ ObjC_Callbacks/ObjC_Callbacks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC_Callbacks
+//
+//  Created by EvilKernel on 11/14/14.
+//  Copyright (c) 2014 Zerogravity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        [[NSRunLoop currentRunLoop] run];
+    }
+    return 0;
+}
